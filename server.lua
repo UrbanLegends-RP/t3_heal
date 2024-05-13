1e9936edee23a51712754cdd3a3d51c1d5314ad8
T3D <143357341+UrbanLegends-RP@users.noreply.github.com>
2024-05-12 21:33:39 -0400

    versioncheck

--- server.lua
+++ server.lua
@@ -13,3 +13,17 @@ AddEventHandler('t3_heal:revivePlayer', function()
         TriggerClientEvent('QBCore:Notify', src, 'Not enough money.', 'error')
     end
 end)
+
+
+
+local function CheckVersion()
+	PerformHttpRequest('https://raw.githubusercontent.com/UrbanLegends-RP/t3_heal/main/t3_heal.txt', function(err, newestVersion, headers)
+		local currentVersion = GetResourceMetadata(GetCurrentResourceName(), 'version')
+		if not newestVersion then print("Currently unable to run a version check.") return end
+		local advice = "^1You are currently running an outdated version^7, ^1please update^7"
+		if newestVersion:gsub("%s+", "") == currentVersion:gsub("%s+", "") then advice = '^6You are running the latest version.^7'
+		else print("^3Version Check^7: ^2Current^7: "..currentVersion.." ^2Latest^7: "..newestVersion) end
+		print(advice)
+	end)
+end
+CheckVersion()
